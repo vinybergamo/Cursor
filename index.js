e3c289c0ad0dcd9032b5687368c0bdde62dca063
vinybergamo <vinybergamo@gmail.com>
2022-10-22 10:51:15 -0300

    Create Effect Cursor

---
+++ index.js
@@ -0,0 +1,23 @@
+const cursor = document.querySelector(".cursor");
+var timeout;
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+  cursor.style.display = "block";
+
+  //cursor effects when mouse stopped
+  function mouseStopped() {
+    cursor.style.display = "none";
+  }
+  clearTimeout(timeout);
+  timeout = setTimeout(mouseStopped, 5000);
+});
+
+//cursor effects when mouseout
+document.addEventListener("mouseout", () => {
+  cursor.style.display = "none";
+});
